742ea939930a20fad154325f75cebafacb42820d
sasp <sasp@1pei.com.cn>
2017-06-12 09:12:56 +0800

    FIX

--- line-list.php
+++ line-list.php
@@ -7,7 +7,7 @@ class Node {
 	public $next;
 
 	/* Node对象 next属性是下一个Node对象*/
-	public function __construct($data, $next) {
+	public function __construct($data = null, $next = null) {
 		$this->data = $data;
 		$this->next = $next;
 	}
@@ -36,9 +36,9 @@ class LinkList {
 		$this->next = null;
 	}
 
-	/* 构造一个链表 */
+	/* 构造一个链表 整表创建 */
 	/*@param $num 线性表的大小 */
-	public function createList($num) {
+	public function createListHead($num) {
 		if($num > self::MAXLENGTH) 
 			return false;
 		for($i = 1;$i <= $num;$i++) {
@@ -49,7 +49,13 @@ class LinkList {
 	}
 
 	public function createListTail($num) {
-		
+		$tail = $this;
+		for($i = 0;$i<$num;$i++) {
+			$data = $i;
+			$node = new Node($data, null);
+			$tail->next = $node;
+			$tail = $node;
+		}
 	}	
 
 	/* 获取第i个元素 */
@@ -62,6 +68,8 @@ class LinkList {
 			$next = $next->next;
 			$j++;
 		}
+		if(!$next || $j > $i) 
+			return false;
 		return $next->data;
 	}
 
@@ -75,7 +83,56 @@ class LinkList {
 	// 	}
 	// 	return $count;
 	// }
+
+	/* 第i个位置插入元素 */
+	public function listInsert($i, $data) {
+		if($i < 1 || $i > self::MAXLENGTH)
+			return false;	
+		$j = 1;
+		$next = $this->next;
+		while($next && $j < $i) {
+			$next = $next->next;
+			$j++;
+		}
+		if(!$next || $j > $i) 
+			return false;
+		$node = new Node($data);
+		$node->next = $next->next;
+		$next->next = $node;
+	}
+
+	/* 删除第i个元素 */
+	public function ListDelete($i, &$e) {
+		if($i < 1 || $i > self::MAXLENGTH)
+			return false;	
+		$j = 1;
+		$next = $this->next;
+		while ($next && $j < ($i - 1)) {
+			$next = $next->next;
+			$j++;
+		}
+		$q = $next->next;
+		$p = $q->next;
+		$next->next = $p;
+		$e = $q->data;
+	}
+
+	/* 整表删除 */
+	public function clearList() {
+		$p = $this->next;
+		while($p) {
+			$node = $p->next;
+			unset($p);
+			$p = $node;
+		}
+	}
 }
 
 $list = LinkList::getInstance();
-$list->createList(5);
+$list->createListTail(5);
+// echo $list->getELem(3);
+// $list->listInsert(3,3.5);
+// $list->ListDelete(2,$e);
+// echo $e;
+$list->clearList();
+// var_export(get_defined_vars());
